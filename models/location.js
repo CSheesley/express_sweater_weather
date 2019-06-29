96864c988006caa0ce64a5be0bde184d0ddf1184
Corey Sheesley <corey.sheesley@gmail.com>
2019-06-29 13:47:02 -0600

    Creates Location Model - updates seed file accordingly

---
+++ models/location.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Location = sequelize.define('Location', {
+    city: DataTypes.STRING,
+    state: DataTypes.STRING,
+    latitude: DataTypes.FLOAT,
+    longitude: DataTypes.FLOAT
+  }, {});
+  Location.associate = function(models) {
+    // associations can be defined here
+  };
+  return Location;
+};
