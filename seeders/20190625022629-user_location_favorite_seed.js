97eb0ef5f2a7aa100580cbb2fb1e9976ce2d6901
Corey Sheesley <corey.sheesley@gmail.com>
2019-06-30 12:38:38 -0600

    Creates Favorite model, migration, and adds to seeder file.

---
+++ seeders/20190625022629-user_location_favorite_seed.js
@@ -0,0 +1,60 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+    await queryInterface.bulkInsert('Locations', [
+      {
+        id: 1,
+        city: 'golden',
+        state: 'co',
+        latitude: 39.7555,
+        longitude: -105.2211,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 2,
+        city: 'jackson',
+        state: 'wy',
+        latitude: 43.4799291,
+        longitude: -110.7624282,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+
+    await queryInterface.bulkInsert('Users', [
+      {
+        id: 1,
+        email: 'user@email.com',
+        password: 'password',
+        apiKey: 'abc123',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+
+    await queryInterface.bulkInsert('Favorites', [
+      {
+        userId: 1,
+        locationId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        userId: 1,
+        locationId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+
+    await queryInterface.bulkDelete('Users', null, {});
+    await queryInterface.bulkDelete('Locations', null, {});
+    await queryInterface.bulkDelete('Favorites', null, {});
+  }
+};
